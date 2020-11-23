5819125e91b1ca89a7c67c8bc59bfc43f2b7700c
shmuly levitin <shmuly11@gmail.com>
2020-11-23 20:12:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,10 +3,11 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   sorted_data = {}
   data.each do |key1, value1|
+    binding.pry
     value1.each do |key2, value2|
       sorted_data[{:value2 => {}}]
     
   end
 end
-binding.pry
+
 end
