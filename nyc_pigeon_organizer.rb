3fc764fba9daea96beb976ace1936b3d7ec3993b
shmuly levitin <shmuly11@gmail.com>
2020-11-23 21:51:43 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,23 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
+  sorted_data = {}
+  data.each do |key, value|
+    value.each do |key2, value2|
+      value2.each do |name|
+       if !sorted_data[name]
+      sorted_data[name] = {}
+       end
+       if !sorted_data[name][key]
+         sorted_data[name][key] = []
+       end
+      sorted_data[name][key].push(key2.to_s)
+      # binding.pry
+      end
+      
+        
+    end
+  end
+#binding.pry
+sorted_data
 end
